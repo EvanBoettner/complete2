97379f5460eaa20db480ebed2f320e88e9476d8f
Sophia Toscano <stoscano@icstars.org>
2022-12-06 10:54:01 -0600

    t1-css

--- v2/src/pages/Home/index.js
+++ v2/src/pages/Home/index.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Navbar from "../../components/Navbar";
-import HomeImage1 from '../../components/assets/ResearchPark.jpg';
-import HomeImage2 from '../../components/assets/WaukeshaCampus.jpg';
-import RocketLogo from '../../components/assets/rblogo.svg';
-import './index.css';
-import '../../App.scss';
+import HomeImage1 from "../../components/assets/ResearchPark.jpg";
+import HomeImage2 from "../../components/assets/WaukeshaCampus.jpg";
+import RocketLogo from "../../components/assets/rblogo.svg";
+import "./index.css";
+import "../../App.scss";
+import { Container } from "react-bootstrap";
 
 const Home = () => {
   return (
@@ -20,16 +21,24 @@ const Home = () => {
             <img className="Logo" src={RocketLogo} alt="RocketBookingLogo" />
           </center>
         </div>
-        <h1 className="location__prompt">Select A Location</h1>
+        <div className="card">
+          <h1 className="location__prompt">
+            Improve productivity and collaboration <br></br>here at GE
+            Healthcare <br></br>
+            by booking a cubicle space at one of our two locations<br></br>
+            using Rocket Booking!
+          </h1>
+        </div>
         <div className="sliderr">
           <div className="Image1">
-            <h3>Research Park</h3>
+            <h3 className="Rpark">Research Park</h3>
             <Link to="/home/wings">
               <img src={HomeImage1} alt="research"></img>
             </Link>
           </div>
           <div className="Image2">
-            <h3>Waukesha Campus</h3>
+            <h3 className="Wcampus">Waukesha Campus</h3>
+
             <Link to="/home/rbooking">
               <img src={HomeImage2} alt="waukesha"></img>
             </Link>
